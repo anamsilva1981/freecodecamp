f337c36e683d9fcf23838c7d9dfed3809b00aee3
Ana Maria <ana.nikoly@gmail.com>
2022-01-28 19:27:29 -0300

    atividades 50 a 60

---
+++ 056.js
@@ -0,0 +1,11 @@
+// Atribuição com um valor retornado
+// Se você se lembrar de nossa discussão sobre Armazenando Valores com o Operador de Atribuição , tudo à direita do sinal de igual é resolvido antes que o valor seja atribuído. Isso significa que podemos pegar o valor de retorno de uma função e atribuí-lo a uma variável.
+
+// Suponha que predefinimos uma função sumque soma dois números, então:
+
+// ourSum = sum(5, 12);
+// chamará a sumfunção, que retorna um valor de 17e o atribui à ourSumvariável.
+
+// Chame a processArgfunção com um argumento de 7e atribua seu valor de retorno à variável processed.
+
+
